df622c72ce1c1961efa368a5e9ca294619645d44
mwild95 <m.wild-13@student.lboro.ac.uk>
2015-03-20 21:36:08 +0000

    Begun The Create request page

---
+++ Timetabling06/ModelViews/ModuleDepartment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Timetabling06.Models;
+
+namespace Timetabling06.ModelViews
+{
+    public class ModuleDepartment
+    {
+        public string code { get; set; }
+        public string username { get; set; }
+        public ICollection<module> modules { get; set; }
+        public ICollection<building> buildings { get; set; }
+        public ICollection<room> rooms { get; set; }
+        public ICollection<round> rounds { get; set; }
+    }
+}
