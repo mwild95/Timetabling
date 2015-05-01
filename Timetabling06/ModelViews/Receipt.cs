244a5810c1ea54b21e220cb6ff7250b135c0065a
mwild95 <m.wild-13@student.lboro.ac.uk>
2015-05-01 15:34:09 +0100

    Reciept Class Added

---
+++ Timetabling06/ModelViews/Receipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Timetabling06.Models;
+
+namespace Timetabling06.ModelViews
+{
+    public class Receipt
+    {
+        public string moduleCode { get; set; }
+        public string moduleTitle { get; set; }
+        public round submittedIn { get; set; }
+        public string department { get; set; }
+        public createObjects[] requests { get; set; }
+
+    }
+}
