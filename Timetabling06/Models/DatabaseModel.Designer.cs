e02de764f87a0bbf7185232fab3d72493b774e68
mwild95 <mwild95@live.co.uk>
2015-05-01 15:09:51 +0100

    Database Changes

--- Timetabling06/Models/DatabaseModel.Designer.cs
+++ Timetabling06/Models/DatabaseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model '\\psf\Home\Desktop\Timetabling06 - Copy\Timetabling06\Models\DatabaseModel.edmx'. 
+// T4 code generation is enabled for model '\\psf\Home\Desktop\Timetabling06 - Copy\Timetabling06\Models\DatabaseModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
